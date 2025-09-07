ee11919f50558983a2e0726f522e07a352fe7c5a
Chadni Rathi <chandani.rathi@gmail.com>
2025-09-08 00:13:24 +0530

    added tailwindcss support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { ripple } from 'vite-plugin-ripple';
 
 export default defineConfig({
-	plugins: [ripple()],
+	plugins: [ripple(), tailwindcss()],
 	server: {
 		port: 3000,
 	},
